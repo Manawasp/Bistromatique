dabfac6ea5471a7d532fe2aea677f7138f1976f6
Kyndt Clovis <clovis.kyndt@epitech.eu>
2013-04-11 17:29:18 +0200

    bigint_of_string

--- Data.hs
+++ Data.hs
@@ -3,7 +3,7 @@
 --- infinite number ---
 data Base = Binary | Octal | Decimal | Hexadecimal
 data Sign = Positive | Negative
-data Bigint = String Base Sign
+data Bigint = Bigint String Base Sign
 
 --- Tree of expression ---
 data Arith_expr
@@ -21,5 +21,47 @@ data Lexer
   | Lex Token Lexer
 
 --  !__DATA__ --
-sum str = str
-bigint_of_string str = str
+
+--- view Data  --
+showSign :: Sign -> String
+showSign Positive = "Positive"
+showSign Negative = "Negative"
+instance Show Sign where
+  show = showSign
+
+showBase :: Base -> String
+showBase Hexadecimal = "Hexadecimal"
+showBase Binary = "Binary"
+showBase Octal = "Octal"
+showBase Decimal = "Decimal"
+instance Show Base where
+  show = showBase
+
+showBigint :: Bigint -> String
+showBigint (Bigint num base sign) = "(" ++ (show num) ++ " : " ++ (show base) ++ " : " ++ (show sign) ++ ")"
+instance Show Bigint where
+  show = showBigint
+
+--- string to bigint ---
+--
+get_sign :: String -> Sign
+get_sign ('-':_) = Negative
+get_sign _ = Positive
+
+get_base :: String -> Base
+get_base ('-':q) = get_base q
+get_base ('0':'x':_) = Hexadecimal
+get_base ('0':'b':_) = Binary
+get_base ('0':_) = Octal
+get_base _ = Decimal
+
+get_num :: String -> String
+get_num ('-':q) = get_num q
+get_num ('x':q) = get_num q
+get_num ('0':q) = get_num q
+get_num str = str
+
+bigint_of_string :: String -> Bigint
+bigint_of_string str = Bigint (get_num str) (get_base str) (get_sign str)
+
+--
